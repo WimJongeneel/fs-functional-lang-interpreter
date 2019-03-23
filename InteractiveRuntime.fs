4a2167ed0b6b2445ef79fab01c62571c55249709
wimjjj <wim.jongeneel1@gmail.com>
2019-03-23 12:47:50 +0100

    add interactive mode

---
+++ InteractiveRuntime.fs
@@ -0,0 +1,59 @@
+module InteractiveRuntime
+
+open AST
+open Runtime
+open Microsoft.FSharp.Text.Lexing
+open System.Text
+
+let evalInput (mem: Memory) (code: string): Memory * MemoryValue = 
+  let ast: Expression list = Parser.start Lexer.tokenstream <| LexBuffer<char>.FromString code
+  let mutable m1: Memory = mem
+  let mutable res = Unit ()
+  List.map (fun e -> (let m2, r = evalExpression m1 e
+                      m1 <- m2;
+                      res <- r;
+                      printfn "-- %A" r;)) ast |> ignore
+  m1, res
+
+let printWelcome () = 
+  printf "-- REPL ready\n--\n"
+  printf "-- commands:\n"
+  printf "--   type ':mem' to inspect the runtimes memory\n"
+  printf "--   type ':store [ID]' to store the last result \n"
+  printf "--   type ':save [FILE]' to save your code in a file \n"
+  printf "--   type ':close' to close the session \n"
+  printf "--\n-- happy coding\n"
+
+let loop () = 
+  let mutable mem: Memory = [Map.empty]
+  let mutable res = Unit ()
+  let mutable running = true
+  let code = StringBuilder ()
+
+  printWelcome ()
+
+  while running do
+    let input = System.Console.ReadLine()
+    if input = ":mem" 
+      then 
+        printfn "%A" mem
+      elif input.StartsWith ":store "
+      then
+        let name = input.Substring 7
+        mem <- writeMemory mem name res
+        printfn "-- Unit ()"
+      elif input = ":close"
+      then
+        running <- false
+        printfn "-- closing the session"
+      elif input.StartsWith ":save "
+      then
+        let name = input.Substring 6
+        System.IO.File.WriteAllText("./repl/" + name, code.ToString())
+        printfn "-- saved your input at ./repl/%s" name
+      else 
+        code.AppendLine(input) |> ignore
+        let m1, r = evalInput mem input
+        mem <- m1
+        res <- r
+  ()
--- Program.fs
+++ Program.fs
@@ -3,15 +3,19 @@ open Microsoft.FSharp.Text.Lexing
 open AST
 open Runtime
 open System.IO
+open InteractiveRuntime
 
 [<EntryPoint>]
 let main argv =
 
-  let code = File.ReadAllText argv.[0]
+  if argv.[0] = "-i" then
+    loop ()
+  else
+    let code = File.ReadAllText argv.[0]
 
-  let lexbuf = LexBuffer<char>.FromString code
-  let ast: Expression list = Parser.start Lexer.tokenstream lexbuf
-  evalExpressions ast
+    let lexbuf = LexBuffer<char>.FromString code
+    let ast: Expression list = Parser.start Lexer.tokenstream lexbuf
+    evalExpressions ast
 
 
   0 
