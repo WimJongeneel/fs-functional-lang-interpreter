9aa520c611f3e55f9a44cdf56b8d67ad99923874
wimjjj <wim.jongeneel1@gmail.com>
2019-04-02 19:15:14 +0200

    fix scoping of lambda param; format documemts

--- AST.fs
+++ AST.fs
@@ -4,50 +4,50 @@ open System
 open System.Linq.Expressions
 
 type MemoryValue =
-| String of string
-| Int of int
-| Bool of bool
-| LambdaExpr of scope: Memory<MemoryValue> * exprs: Expression list * param: string * _rec: string option
-| Unit of unit
-| Array of MemoryValue []
-| Object of Map<string, MemoryValue>
+| String            of string
+| Int               of int
+| Bool              of bool
+| LambdaExpr        of scope: Memory<MemoryValue> * exprs: Expression list * param: string * _rec: string option
+| Unit              of unit
+| Array             of MemoryValue []
+| Object            of Map<string, MemoryValue>
 
 and Expression = 
-| Value of MemoryValue
-| Read of id: string
-| Write of id: string * expr: Expression * _rec: bool * _type: Type
-| Lambda of param: string * paramType: Type * exprs: Expression list
-| Apply of func: Expression * param: Expression
-| Nested of Expression
-| Echo of Expression
-| Equals of left: Expression * rigth: Expression
-| NotEquals of left: Expression * rigth: Expression
-| Plus of left: Expression * rigth: Expression
-| Min of left: Expression * rigth: Expression
-| Times of left: Expression * rigth: Expression
-| Divide of left: Expression * rigth: Expression
-| Condition of con: Expression * _then: Expression * _else: Expression
-| And of left: Expression * rigth: Expression
-| Or of left: Expression * rigth: Expression
-| ArrayInit of Expression list
-| ArrayGet of list: Expression * index: Expression
-| ObjectInit of (string * Expression) list
-| ObjectGet of obj: Expression * key: string
-| ObjectCopyWith of obj: Expression * newValue: (string * Expression)
-| Open of string
+| Value             of MemoryValue
+| Read              of id: string
+| Write             of id: string * expr: Expression * _rec: bool * _type: Type
+| Lambda            of param: string * paramType: Type * exprs: Expression list
+| Apply             of func: Expression * param: Expression
+| Nested            of Expression
+| Echo              of Expression
+| Equals            of left: Expression * rigth: Expression
+| NotEquals         of left: Expression * rigth: Expression
+| Plus              of left: Expression * rigth: Expression
+| Min               of left: Expression * rigth: Expression
+| Times             of left: Expression * rigth: Expression
+| Divide            of left: Expression * rigth: Expression
+| Condition         of con: Expression * _then: Expression * _else: Expression
+| And               of left: Expression * rigth: Expression
+| Or                of left: Expression * rigth: Expression
+| ArrayInit         of Expression list
+| ArrayGet          of list: Expression * index: Expression
+| ObjectInit        of (string * Expression) list
+| ObjectGet         of obj: Expression * key: string
+| ObjectCopyWith    of obj: Expression * newValue: (string * Expression)
+| Open              of string
 
 and Memory<'m> = List<Map<string, 'm>>
 
 and Literal = 
-| StringLiteral of string
-| IntLiteral of int
-| BoolLiteral of bool
+| StringLiteral     of string
+| IntLiteral        of int
+| BoolLiteral       of bool
 | UnitLiteral
 
 and Type = 
-| NamedType of string
-| LiteralType of Literal
-| FuncType of Type * Type
+| NamedType         of string
+| LiteralType       of Literal
+| FuncType          of Type * Type
 
 let readMemory<'m> (mem: Memory<'m>) (id: string) =
   List.tryPick (fun scope -> if Map.containsKey id scope then scope.[id] |> Some else None) mem
--- TypeChecker.fs
+++ TypeChecker.fs
@@ -3,12 +3,12 @@ module TypeChecker
 open System
 open AST
 
-type TypeEntry = 
+type TypeEntry =
   | UnitType
-  | IntType of int option
-  | BoolType of bool option
-  | StringType of string option
-  | FunctionType of param: TypeEntry * ret: TypeEntry
+  | IntType       of int option
+  | BoolType      of bool option
+  | StringType    of string option
+  | FunctionType  of param: TypeEntry * ret: TypeEntry
 
 let rec typeCheckExpression (mem: Memory<TypeEntry>) (expr: Expression): Memory<TypeEntry> * TypeEntry =
   match expr with
@@ -87,16 +87,18 @@ let rec typeCheckExpression (mem: Memory<TypeEntry>) (expr: Expression): Memory<
                                | (IntType _, IntType _)         -> m2, BoolType None
                                | (UnitType _, UnitType _)       -> m2, BoolType None
                                | _                              -> Exception <| sprintf "Type error with '%A' != '%A'" l r |> raise
-  | Lambda (_, t, eb)          -> mem, FunctionType (typeToTypeEntry t, typeCheckFuncBody mem eb)
+  | Lambda (p, t, eb)          -> mem, FunctionType (typeToTypeEntry t, typeCheckFuncBody mem eb p <| typeToTypeEntry t)
   | Apply (e, pe)           -> let m1, f = typeCheckExpression mem e
                                let m2, pt = typeCheckExpression m1 pe
                                match f with
-                               | FunctionType (p, r) when isAssignable pt p -> m2, r
+                               | FunctionType (p, r) when isAssignable p pt -> m2, r
                                | FunctionType (p, _)                        -> Exception <| sprintf "Given value '%A' is not assignable to param '%A'" pt p |> raise
                                | _                                          -> Exception <| sprintf "No not callable:  '%A'" e |> raise
+  // Arrays
+  // Objects
   | _                       -> Exception <| sprintf "No type for %A" expr |> raise
 
-and isAssignable (expected: TypeEntry) (given: TypeEntry) = 
+and isAssignable (expected: TypeEntry) (given: TypeEntry) =
   match (expected, given) with
   | (StringType s1, StringType s2) when s1.IsSome && s2.IsSome -> s1.Value = s2.Value
   | (StringType s1, StringType _) when s1.IsNone              -> true
@@ -108,7 +110,7 @@ and isAssignable (expected: TypeEntry) (given: TypeEntry) =
   | (FunctionType (p1, r1), FunctionType(p2, r2))             -> isAssignable p2 p1 && isAssignable r1 r2
   | _                                                         -> false
 
-and narrowIfScope (mem: Memory<TypeEntry>) (pred: Expression) : Memory<TypeEntry> = 
+and narrowIfScope (mem: Memory<TypeEntry>) (pred: Expression) : Memory<TypeEntry> =
   match pred with
   | Equals (l, r) -> match (l, r) with
                      | (Value _, Read id) -> let _, t = typeCheckExpression mem l
@@ -124,26 +126,27 @@ and narrowIfScope (mem: Memory<TypeEntry>) (pred: Expression) : Memory<TypeEntry
 and typeToTypeEntry (t: Type) =
   match t with
   | LiteralType lt      -> match lt with
-                           | StringLiteral s -> StringType <| Some s
-                           | IntLiteral i    -> IntType <| Some i
-                           | BoolLiteral b   -> BoolType <| Some b
+                           | StringLiteral s -> StringType  <| Some s
+                           | IntLiteral i    -> IntType     <| Some i
+                           | BoolLiteral b   -> BoolType    <| Some b
                            | UnitLiteral     -> UnitType
   | NamedType nt        -> match nt with
-                           | "string" -> StringType None
-                           | "int"    -> IntType None
-                           | "bool"   -> BoolType None
-                           | _        -> UnitType
+                           | "string"        -> StringType None
+                           | "int"           -> IntType None
+                           | "bool"          -> BoolType None
+                           | _               -> UnitType
   | FuncType (p, r)     -> FunctionType (typeToTypeEntry p, typeToTypeEntry r)
 
-and typeCheckFuncBody (mem: Memory<TypeEntry>) (exprs: Expression list): TypeEntry = 
+and typeCheckFuncBody (mem: Memory<TypeEntry>) (exprs: Expression list) (paramAlias: string) (param: TypeEntry): TypeEntry =
   let mutable m1: Memory<TypeEntry> = Map.empty :: mem
   let mutable t : TypeEntry = UnitType
+  m1 <- writeMemory mem paramAlias param
   List.map (fun e -> (let m2, t1 = typeCheckExpression m1 e
                     m1 <- m2;
                     t <- t1)) exprs |> ignore
   t
 
-let typeCheckExpressions (exprs: Expression list) = 
+let typeCheckExpressions (exprs: Expression list) =
   let mutable mem: Memory<TypeEntry> = [Map.empty]
   List.map (fun e -> (let m1, _ = typeCheckExpression mem e
                     mem <- m1;)) exprs |> ignore
